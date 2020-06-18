6831e9e77f52908774019cbd23345a5201c49a68
heonseok <heonseok.ha@gmail.com>
2020-06-19 02:01:05 +0900

    change commit option

--- main.py
+++ main.py
@@ -24,6 +24,9 @@ parser.add_argument('--early_stop_observation_period', type=int, default=20)
 parser.add_argument('--repeat_idx', type=int, default=0)
 parser.add_argument('--gpu_id', type=int, default=0)
 parser.add_argument('--attack_type', type=str, default='stat', choices=['stat', 'black', 'white'])
+
+parser.add_argument('--train_target_classifier', type=str2bool, default='0')
+parser.add_argument('--train_attack_model', type=str2bool, default='1')
 args = parser.parse_args()
 
 torch.cuda.set_device(args.gpu_id)
@@ -41,17 +44,21 @@ subset0 = Subset(trainset, range(args.setsize))
 subset1 = Subset(trainset, range(args.setsize, 2 * args.setsize))
 subset2 = Subset(trainset, range(2 * args.setsize, 3 * args.setsize))
 
-cls_datasets = {
-    'train': subset0,
-    'valid': subset1,
-    'test': subset2,
-}
-for dataset_type, dataset in cls_datasets.items():
-    print('Cls {:<5} : {}'.format(dataset_type, len(dataset)))
-# cls_model = Classifier(args)
-# cls_model.train(cls_datasets['train'], cls_datasets['valid'])
-# cls_model.test(cls_datasets['test'])
-# cls_model.log_prediction_score(cls_datasets)
+if args.train_target_classifier:
+    cls_datasets = {
+        'train': subset0,
+        'valid': subset1,
+        'test': subset2,
+    }
+    for dataset_type, dataset in cls_datasets.items():
+        print('Cls {:<5} : {}'.format(dataset_type, len(dataset)))
 
-attack_model = Attacker(args)
-# attack_model
+    cls_model = Classifier(args)
+    cls_model.train(cls_datasets['train'], cls_datasets['valid'])
+    cls_model.test(cls_datasets['test'])
+    cls_model.log_prediction_score(cls_datasets)
+
+if args.train_attack_model:
+    attack_model = Attacker(args)
+    attack_model.
+    # attack_model
