bb291e35bc47136216888671c9d1e5a3485e2432
heonseok <heonseok.ha@gmail.com>
2020-08-04 15:06:20 +0900

    Done: collate classification/attack; Todo: class/membership/style;

--- visualize_results.py
+++ visualize_results.py
@@ -1,29 +1,26 @@
 import os
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import sys
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 base_path = os.path.join('/mnt/disk1/heonseok/MPMLD')
 if not os.path.exists('Figs'):
     os.mkdir('Figs')
 
+REPEAT = 5
 
-def collate_reconstructions(dataset, description, model):
+
+# -------------------------------------------------------------------------------------------------------------------- #
+def collate_reconstructions(dataset, description, model, recon_type_list):
     model_path = os.path.join(base_path, dataset, description, model, 'reconstruction')
-    recon_type_list = [
-        'cb_mb.png',
-        'cz_mb.png',
-        'cb_mz.png',
-    ]
 
     plt.figure(1, figsize=(9, 6))
     for recon_idx, recon_type in enumerate(recon_type_list):
-        for repeat_idx in range(5):
-            plt.subplot(3, 5, repeat_idx + recon_idx * 5 + 1)
-            plt.imshow(mpimg.imread(os.path.join(model_path, 'repeat{}'.format(repeat_idx), recon_type)))
+        for repeat_idx in range(REPEAT):
+            plt.subplot(len(recon_type_list), REPEAT, repeat_idx + recon_idx * REPEAT + 1)
+            plt.imshow(mpimg.imread(os.path.join(model_path, 'repeat' + str(repeat_idx), recon_type + '.png')))
             plt.xticks([])
             plt.yticks([])
 
@@ -42,7 +39,7 @@ def collate_reconstructions(dataset, description, model):
 def collate_disentanglement_result(dataset, description, model):
     model_path = os.path.join(base_path, dataset, description, model, 'reconstruction')
     df = pd.DataFrame()
-    for repeat_idx in range(5):
+    for repeat_idx in range(REPEAT):
         repeat_path = os.path.join(model_path, 'repeat{}'.format(repeat_idx), 'acc.npy')
         acc_dict = np.load(repeat_path, allow_pickle=True).item()
         df = df.append(acc_dict, ignore_index=True)
@@ -63,10 +60,72 @@ def collate_disentanglement_result(dataset, description, model):
     os.system('rclone copy -P {} remote:{}'.format(img_path, drive_path))
 
 
+def collate_classification_result(dataset, description, model, recon_type_list):
+    model_path = os.path.join(base_path, dataset, description, model, 'classification', 'ResNet18')
+    df = pd.DataFrame()
+    for recon_idx, recon_type in enumerate(recon_type_list):
+        for repeat_idx in range(REPEAT):
+            repeat_path = os.path.join(model_path, recon_type, 'repeat' + str(repeat_idx), 'acc.npy')
+            acc_dict = np.load(repeat_path, allow_pickle=True).item()
+            for dataset_type in ['train', 'valid', 'test']:
+                df = df.append({'recon': recon_type, 'dataset': dataset_type, 'acc': acc_dict[dataset_type]},
+                               ignore_index=True)
+
+    sns.boxplot(x='recon', y='acc', hue='dataset', data=df)
+    plt.ylim(0., 1.01)
+    plt.tight_layout()
+
+    img_dir = os.path.join('Figs', dataset, description, 'classification_collated')
+    if not os.path.exists(img_dir):
+        os.makedirs(img_dir)
+    img_path = os.path.join(img_dir, '{}.png'.format(model))
+    plt.savefig(img_path)
+    plt.close()
+
+    drive_path = os.path.join('Research/MPMLD/', img_dir)
+    os.system('rclone copy -P {} remote:{}'.format(img_path, drive_path))
+
+
+def collate_attack_result(dataset, description, model, recon_type_list):
+    model_path = os.path.join(base_path, dataset, description, model, 'attack', 'ResNet18')
+    attack_type_list = [
+        'black',
+        # 'white',
+    ]
+
+    df = pd.DataFrame()
+    for recon_idx, recon_type in enumerate(recon_type_list):
+        for attack_type in attack_type_list:
+            for repeat_idx in range(REPEAT):
+                repeat_path = os.path.join(model_path, recon_type, attack_type, 'repeat' + str(repeat_idx), 'acc.npy')
+                acc_dict = np.load(repeat_path, allow_pickle=True).item()
+                df = df.append({'recon': recon_type, 'attack_type': attack_type, 'acc': acc_dict['test']},
+                               ignore_index=True)
+
+    sns.boxplot(x='recon', y='acc', hue='attack_type', data=df)
+    plt.ylim(0.4, 0.8)
+    plt.tight_layout()
+
+    img_dir = os.path.join('Figs', dataset, description, 'attack_collated')
+    if not os.path.exists(img_dir):
+        os.makedirs(img_dir)
+    img_path = os.path.join(img_dir, '{}.png'.format(model))
+    plt.savefig(img_path)
+    plt.close()
+
+    drive_path = os.path.join('Research/MPMLD/', img_dir)
+    os.system('rclone copy -P {} remote:{}'.format(img_path, drive_path))
+
+
 # -------------------------------------------------------------------------------------------------------------------- #
 def main():
     dataset = 'SVHN'
     description = '0803weights'
+    recon_type_list = [
+        'cb_mb',
+        'cb_mz',
+        'cz_mb',
+    ]
     model_list = [
         'VAE1e-06_z64_setsize10000_lr0.001_ref0.1_rw1.0_cc0.0_cm0.0_mc0.0_mm0.0',
 
@@ -80,8 +139,10 @@ def main():
 
     for model in model_list:
         print(model)
-        # collate_reconstructions(dataset, description, model)
-        collate_disentanglement_result(dataset, description, model)
+        # collate_reconstructions(dataset, description, model, recon_type_list)
+        # collate_disentanglement_result(dataset, description, model)
+        # collate_classification_result(dataset, description, model, recon_type_list)
+        # collate_attack_result(dataset, description, model, recon_type_list)
 
 
 if __name__ == '__main__':
